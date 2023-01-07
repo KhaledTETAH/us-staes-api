134ad6d615bde3d5437175071cd434a1cd46bc46
Khaled TETAH <TKhaled@Smartest.dz>
2023-01-07 17:27:05 +0100

    us api project created

---
+++ src/main/java/com/info/usstates/UsStatesApplication.java
@@ -0,0 +1,13 @@
+package com.info.usstates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsStatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsStatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/info/usstates/UsStatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.usstates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsStatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
